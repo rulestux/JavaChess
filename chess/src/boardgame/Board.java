61ecc5fc7115517a1030fc1c9e3c6dc5ed724840
rulestux <rulestux@gmail.com>
2025-12-12 13:08:10 -0300

    Piece movement

--- chess/src/boardgame/Board.java
+++ chess/src/boardgame/Board.java
@@ -39,12 +39,27 @@ public class Board {
         return pieces[position.getRow()][position.getColumn()];
     }
 
+    // método para inserção de peça numa posição:
     public void placePiece(Piece piece, Position position) {
         if (thereIsAPiece(position)) {
             throw new BoardException("There is already a piece on position " + position + ".");
         }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+    // método para remoção de peça de uma posição:
+    public Piece removePiece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the board.");
+        }
+        if (piece(position) == null) {
+            return null;
+        }
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+        return aux;
     }
 
     private boolean positionExists(int row, int column) {
